591e389ef2cad7f7101fc3cf28722448de0a3607
Adam McCurdy <adam.mccurdy@gmail.com>
2017-01-25 10:52:54 -0600

    moving the check for broken devs to outside the 'fix' part

--- prodStateFixer.py
+++ prodStateFixer.py
@@ -4,13 +4,13 @@ import sys
 from Products.ZenUtils.ZenScriptBase import ZenScriptBase
 from transaction import commit
 
-usage = 'prodStateFixer.py <check|clean> \n'
+usage = 'prodStateFixer.py <check|fix> \n'
 
 # check args
 if len(sys.argv) != 2:
     print usage
     sys.exit(1)
-elif sys.argv[1] not in ['check', 'clean']:
+elif sys.argv[1] not in ['check', 'fix']:
     print usage
     sys.exit(1)
    
@@ -25,17 +25,17 @@ for d in dmd.Devices.getSubDevicesGen():
     try:
       dps = d.getProductionState()
     except:
-      print ">>> Device %s is missing it's production state" % d.id
+      print '>>> Device %s is missing it's production state' % d.id
       broken_devices.append(d)
 
-# if the arg is 'clean' then set their prodState to 1000 (production)
-if sys.argv[1] == 'clean':
-    if len(broken_devices) == 0:
-        print 'No devices to fix.'
-        sys.exit(1)
-    else: 
-        print 'Cleaning devices...'
-        for d in broken_devices:
-            print "Setting %s to production" % d.id
-            d._setProductionState(1000)
-            commit()
+if len(broken_devices) == 0:
+    print 'No devices to fix.'
+    sys.exit(1)
+      
+# if the arg is 'fix' then set their prodState to 1000 (production)
+if sys.argv[1] == 'fix':
+    print 'Cleaning devices...'
+    for d in broken_devices:
+        print 'Setting %s to production' % d.id
+        d._setProductionState(1000)
+        commit()
